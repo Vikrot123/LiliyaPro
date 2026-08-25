df0eb45fe6faa625634580150ea2f6d73e67ce13
Vikrot123 <Vikrot@i.ua>
2026-08-25 21:35:46 +0300

    Core Foundation v1.333: lock observer provider reset identity

--- core/src/test/kotlin/pro/liliya/core/DefaultRuntimeKnowledgeLifecycleObserverProviderContractTest.kt
+++ core/src/test/kotlin/pro/liliya/core/DefaultRuntimeKnowledgeLifecycleObserverProviderContractTest.kt
@@ -24,4 +24,28 @@ class DefaultRuntimeKnowledgeLifecycleObserverProviderContractTest {
             provider.observerRegistry()
         )
     }
+    @Test
+    fun provider_follows_holder_registry_after_reset() {
+        val holder =
+            DefaultRuntimeKnowledgeLifecycleObserverRegistryHolder()
+
+        val provider =
+            DefaultRuntimeKnowledgeLifecycleObserverProvider(holder)
+
+        val beforeReset = provider.observerRegistry()
+
+        holder.reset()
+
+        val afterReset = provider.observerRegistry()
+
+        assertSame(
+            holder.registry(),
+            afterReset
+        )
+        kotlin.test.assertNotSame(
+            beforeReset,
+            afterReset
+        )
+    }
+
 }
