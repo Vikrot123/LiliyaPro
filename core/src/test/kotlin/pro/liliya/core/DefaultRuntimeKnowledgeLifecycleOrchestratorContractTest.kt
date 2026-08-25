bec04743b4fca14d2e6bcea73ae7d7d35b7d8314
Vikrot123 <Vikrot@i.ua>
2026-08-25 21:14:36 +0300

    Core Foundation v1.330: lock orchestrator result propagation

--- core/src/test/kotlin/pro/liliya/core/DefaultRuntimeKnowledgeLifecycleOrchestratorContractTest.kt
+++ core/src/test/kotlin/pro/liliya/core/DefaultRuntimeKnowledgeLifecycleOrchestratorContractTest.kt
@@ -44,4 +44,48 @@ class DefaultRuntimeKnowledgeLifecycleOrchestratorContractTest {
             result.executionResult.executed
         )
     }
+    @Test
+    fun orchestrator_preserves_successful_execution_result() {
+        val orchestrator =
+            DefaultRuntimeKnowledgeLifecycleOrchestrator(
+                object : RuntimeKnowledgeLifecycleExecutor {
+                    override fun execute(
+                        knowledge: RuntimeKnowledge
+                    ): RuntimeKnowledgeLifecycleExecutionResult {
+                        return RuntimeKnowledgeLifecycleExecutionResult(
+                            executed = true
+                        )
+                    }
+                }
+            )
+
+        val result = orchestrator.process(knowledge())
+
+        assertTrue(
+            result.executionResult.executed
+        )
+    }
+
+    @Test
+    fun orchestrator_preserves_rejected_execution_result() {
+        val orchestrator =
+            DefaultRuntimeKnowledgeLifecycleOrchestrator(
+                object : RuntimeKnowledgeLifecycleExecutor {
+                    override fun execute(
+                        knowledge: RuntimeKnowledge
+                    ): RuntimeKnowledgeLifecycleExecutionResult {
+                        return RuntimeKnowledgeLifecycleExecutionResult(
+                            executed = false
+                        )
+                    }
+                }
+            )
+
+        val result = orchestrator.process(knowledge())
+
+        assertTrue(
+            !result.executionResult.executed
+        )
+    }
+
 }
